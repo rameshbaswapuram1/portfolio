20de2f469f4fccae1b0eb460739d08ace8687140
ramesh <rameshbaswapuram1@gmail.com>
2025-09-11 13:12:32 +0530

    updated

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+  email: "rameshbaswapuram1@gmail.com",
+  phone: "+919701378218",
+  address: "Kosgi, Kosgi, Narayanpet, TS - 509339 ",
+  github: "https://github.com/said7388",
+  facebook: "https://www.facebook.com/abusaid.riyaz/",
+  linkedIn: "https://www.linkedin.com/in/abu-said-bd/",
+  twitter: "https://twitter.com/said7388",
+  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  devUsername: "said7388",
+};
