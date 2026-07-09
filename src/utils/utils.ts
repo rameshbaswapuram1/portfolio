9c62e30b613b1260a31ae5f355ca50f36215d19d
ramesh <rameshbaswapuram1@gmail.com>
2026-07-09 11:47:00 +0530

    update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,5 +3,5 @@ export const links = {
   linkedIn: "https://linkedin.com/in/yourprofile",
   stackOverflow: "https://stackoverflow.com/users/24862926/ramesh-goud",
   resumeDrive:
-    "https://drive.google.com/file/d/1ufasVSxYXby8psdgOY76CWqfA1DmM6q0/view?usp=drive_link",
+    "https://drive.google.com/file/d/1FoukFxrbDuG4bCqlrgJwpxJ0vi1I265s/view?usp=drive_link",
 };
